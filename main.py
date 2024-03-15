e3027105fed37eb90aa27cefe2eb882596df3e8b
RedBit <605761792@qq.com>
2024-03-15 11:03:24 +0800

    use tqdm.contrib.concurrent to manage thread pool

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import math
 import pandas as pd
 from datetime import datetime
 import os
-from threading import Thread, Lock
+from threading import current_thread, Lock
+from tqdm.contrib.concurrent import thread_map
 
 from config import dir_path, save_path, start, end, row_filter_column, file_suffix
 
 
-def thread_process_xlsx(_part_data, _row_filter_column, _start_date, _end_date, _lock, _total):
-    thread_name = Thread.getName()
+def thread_process_xlsx(_part_data: pd.DataFrame, _row_filter_column: str, _start_date: datetime, _end_date: datetime,
+                        _lock: Lock, _total: pd.DataFrame):
+    thread_name = current_thread().getName()
+    # print(f"Thread {thread_name} started")
     new_filtered_data = pd.DataFrame()
     for row in range(0, len(_part_data)):
         selected_row = _part_data.iloc[row]  # get row data, type: Pandas Series
@@ -18,27 +21,30 @@ def thread_process_xlsx(_part_data, _row_filter_column, _start_date, _end_date, 
         if _start_date <= open_date <= _end_date:
             new_filtered_data = new_filtered_data._append(selected_row.transpose())
     _lock.acquire()
-    _total.append(new_filtered_data)
+    _total._append(new_filtered_data)
     _lock.release()
     pass
 
 
-def process_xlsx(_path_to_file, _row_filter_column, _start_date, _end_date):
+def process_xlsx(_path_to_file: str, _row_filter_column: str, _start_date: datetime, _end_date: datetime):
     df = pd.read_excel(_path_to_file)
     total_rows = len(df)
     new_df = pd.DataFrame()
     thread_num = math.ceil(total_rows / 10000)
     lock = Lock()
+    tasks = []
     for thread in range(thread_num):
         start_row = thread * 10000
         end_row = (thread + 1) * 10000
         if end_row > total_rows:
             end_row = total_rows
         part_data = df.iloc[start_row:end_row]
-        Thread(target=thread_process_xlsx,
-               args=[part_data, _row_filter_column, _start_date, _end_date, lock, new_df],
-               name=start_row + '-' + end_row
-               ).start()
+        tasks.append([part_data, _row_filter_column, _start_date, _end_date, lock, new_df])
+    thread_map(lambda x: thread_process_xlsx(*x), tasks, desc="Processing " + _path_to_file, max_workers=8)
+    # Thread(target=thread_process_xlsx,
+    #        args=[part_data, _row_filter_column, _start_date, _end_date, lock, new_df],
+    #        name=start_row + '-' + end_row
+    #        ).start()
     # for row in range(0, len(df)):
     #     selected_row = df.iloc[row]  # get row data, type: Pandas Series
     #     if type(selected_row[_row_filter_column]) == float and math.isnan(selected_row[_row_filter_column]):
